9ee22a107f9cd166c7c6f047de2083cca37448f4
Simon <smkim216@gmail.com>
2021-05-16 16:59:23 -0700

    two-susm

---
+++ Array/easy/TwoSum.scala
@@ -0,0 +1,15 @@
+object Solution {
+    def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+        val map = scala.collection.mutable.Map[Int, Int] ()
+        var count = 0;
+        for(num <- nums){
+            val comp = target-num
+            map.get(comp) match {
+                case None => {map.put(num,count)}
+                case Some(index) => return Array(index,count) 
+            }
+            count = count+1
+        }
+        Array(0,0)
+    }
+}
